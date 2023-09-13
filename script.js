88048917672b2fb48476383b828d1987543a3c1f
Gislaine Jéssica <92621463+Gislaine-J-Silva@users.noreply.github.com>
2023-09-13 00:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -80,9 +80,9 @@ const icone = document.getElementById('icone');
 toggleButton.addEventListener('click', function() {
     const iconeAtual = icone.getAttribute("src");
     
-    if (iconeAtual === "./icons/MoonStars.png") {
-        icone.setAttribute("src", "./icons/Sun.png");
-    } else {
+    if (iconeAtual === "./icons/Sun.png") {
         icone.setAttribute("src", "./icons/MoonStars.png");
+    } else {
+        icone.setAttribute("src", "./icons/Sun.png");
     }
 });
